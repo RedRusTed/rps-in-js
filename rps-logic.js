eb81921026cbacf7deb4fd55c1182d7a7d70c08a
RedRusT <237728949+RedRusTed@users.noreply.github.com>
2026-01-30 11:35:27 +0300

    changing logic completly

--- rps-logic.js
+++ rps-logic.js
@@ -1,53 +1,34 @@
-//rock = 1
-//paper = 2
-//scissors = 3
 let humanScore = 0
 let computerScore = 0
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
+const buttonStart = document.querySelector("#button-start")
+const buttonReset = document.querySelector("#button-reset")
+const buttonRock = document.querySelector("#rock")
+const buttonPaper = document.querySelector("#paper")
+const buttonScissors = document.querySelector("#scissors")
+
+const playerScore = document.querySelector("#player-score")
+const pcScore = document.querySelector("#pc-score")
 
 function getComputerChoice(){
-    let pcChoice = getRandomInt(3) + 1
-    if (pcChoice == 1){
-        return "rock"
-    }else if(pcChoice == 2){
-        return "paper"
-    }else{
-        return "scissors"
+    let int = Math.floor(Math.random() * 3)
+    switch (int){
+        case 0:
+            return "rock"
+        case 1:
+            return "paper"
+        case 2:
+            return "scissors"
     }
 }
 
-function getUserChoice(){
-    let userChoice = prompt("Choose Rock/Paper/Scissors: ").toLowerCase()
-    while (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors"){
-        userChoice = prompt("Choose Rock/Paper/Scissors").toLowerCase()
-    }
+buttonRock.addEventListener("click", () => {getUserChoice("rock")})
+buttonPaper.addEventListener("click", () => {getUserChoice("paper")})
+buttonScissors.addEventListener("click", () => {getUserChoice("scissors")})
 
-    return userChoice
+function getUserChoice(value){
+    console.log("User: " + value)
 }
 
-function playRound(){
-   let user = getUserChoice()
-   let pc = getComputerChoice()
-   
-   if (user == "paper" && pc == "rock"){
-    humanScore += 1
-    console.log("Human Win Round")
-   }else if( user == "rock" && pc == "scissors"){
-    humanScore += 1
-    console.log("Human Win Round")
-   }else if ( user == "scissors" && pc == "paper"){
-    humanScore += 1
-    console.log("Human Win Round")
-   }else if (user == pc){
-    console.log("Draw")
-   }else{
-    computerScore += 1
-    console.log("PC Win Round")
-   }
-}
-
-
-
-
+buttonStart.addEventListener('click', () =>{
+    console.log(getComputerChoice())
+})
